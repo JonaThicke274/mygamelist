774ae9450cbf4a50ed0e1ea19c495e4d9870c5bd
tmunkhb <tmunkhb@gmail.com>
2023-01-07 15:56:24 -0500

    added SavedGames pages without game description or author

--- client/src/pages/SavedGames.js
+++ client/src/pages/SavedGames.js
@@ -1,6 +1,101 @@
-function SavedGames() {
-    return <p>saved games</p>;
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+
+import { getMe, deleteGame } from '../utils/API';
+import Auth from '../utils/auth';
+import { removeGameId } from '../utils/localStorage';
+
+const SavedGames = () => {
+  const [userData, setUserData] = useState({});
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+          return false;
+        }
+
+        const response = await getMe(token);
+
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, [userDataLength]);
+
+  // create function that accepts the game's mongo _id value as param and deletes the game from the database
+  const handleDeleteGame = async (gameId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const response = await deleteGame(gameId, token);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const updatedUser = await response.json();
+      setUserData(updatedUser);
+      // upon success, remove game's id from localStorage
+      removeGameId(gameId);
+    } catch (err) {
+      console.error(err);
+    }
   };
-  
-  export default SavedGames;
-  
+
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved games!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedGames.length
+            ? `Viewing ${userData.savedGames.length} saved ${userData.savedGames.length === 1 ? 'game' : 'games'}:`
+            : 'You have no saved games!'}
+        </h2>
+        <CardColumns>
+          {userData.savedGames.map((game) => {
+            return (
+              <Card key={game.gameId} border='dark'>
+                {game.image ? <Card.Img src={game.image} alt={`The cover for ${game.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{game.title}</Card.Title>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteGame(game.gameId)}>
+                    Delete this Game!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedGames;
