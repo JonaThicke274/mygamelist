aad1adc36f2cc6c0abf06feb4fabd49f274300b8
JonaThicke274 <lipatajonathan274@gmail.com>
2023-01-07 16:19:49 -0500

    corrected name for removeGame mutation in typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = gql`
         login (email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
         saveGame(input: saveGameInput): User
-        removeBook(gameId: String!): User
+        removeGame(gameId: String!): User
     }
 `;
 
