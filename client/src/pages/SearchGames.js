0623781c29e87e6e9cf4a3576e1ad33e48431cf7
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2023-01-06 21:05:11 -0500

    search-bar functionality

--- client/src/pages/SearchGames.js
+++ client/src/pages/SearchGames.js
@@ -1,5 +1,73 @@
-function SearchGames() {
-  return <p>search games</p>;
+import React, {/*{ useState, useEffect }*/} from 'react';
+import { Jumbotron, Container, Col, Form, Button,  } from 'react-bootstrap';
+// import Auth from '../utils/auth';
+
+const SearchGames = () => {
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Search for Games!</h1>
+          <Form>
+          {/* <Form onSubmit={handleFormSubmit}> */}
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name='searchInput'
+                  // value={searchInput}
+                  // onChange={(e) => setSearchInput(e.target.value)}
+                  type='text'
+                  size='lg'
+                  placeholder='Search for a game'
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type='submit' variant='success' size='lg'>
+                  Submit Search
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
+
+      {/* <Container>
+        <h2>
+          {searchedBooks.length
+            ? `Viewing ${searchedBooks.length} results:`
+            : 'Search for a book to begin'}
+        </h2>
+        <CardColumns>
+          {searchedBooks.map((book) => {
+            return (
+              <Card key={book.bookId} border='dark'>
+                {book.image ? (
+                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  {Auth.loggedIn() && (
+                    <Button
+                      disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                      className='btn-block btn-info'
+                      onClick={() => handleSaveBook(book.bookId)}>
+                      {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
+                        ? 'This book has been saved!'
+                        : 'Save this Book!'}
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container> */}
+    </>
+  );
+
+
 };
 
 export default SearchGames;
